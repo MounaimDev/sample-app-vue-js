e6be68f2c69e4a3b6ac99271cd4ca2c828b98710
MOUNAIM <105808794+MounaimDev@users.noreply.github.com>
2023-01-21 10:47:01 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+// app.js
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello, Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
